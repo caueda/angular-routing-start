b1a10b8d6e10028e14c3b3f7de6631883a744b18
carlos.ueda <carlos.ueda@gmail.com>
2020-09-10 06:43:08 -0400

    External Routing module created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './users/users.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,21 +14,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Route, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Route[] = [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id', component: UserComponent}
-  ]},
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent},
-    { path: ':id/edit', component: EditServerComponent}
-  ]},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-];
+
 
 @NgModule({
   declarations: [
@@ -42,7 +32,8 @@ const appRoutes: Route[] = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [ServersService, UsersService],
   bootstrap: [AppComponent]
